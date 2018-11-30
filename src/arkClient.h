10c8df4eed7feafbfe766e3223042a1c06b87586
Chris <chrisjohnsonmail@gmail.com>
2018-11-29 20:01:13 -0600

    fix:  Fix Arduino IDE support
    Added new ArkClient.h in the location that Arduino IDE demands.  This new header just includes the previous header as an adapter layer.

---
+++ src/arkClient.h
@@ -0,0 +1,12 @@
+/**
+ * This file is part of Ark Cpp Crypto.
+ *
+ * (c) Ark Ecosystem <info@ark.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ **/
+
+ // This file is just to provide an adapter for the Arduino IDE to find the public arkClient.h header
+ // There is no include guard in this file.  It is intentional as this is intended to be a "copy" of the arkClient.h header.
+#include "include/cpp-client/arkClient.h"
