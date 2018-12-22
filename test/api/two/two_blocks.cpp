79d27fb316064e80d9a6e189db2309270f8be51e
Chris <chrisjohnsonmail@gmail.com>
2018-12-21 22:00:10 -0600

    refactor:  Add initial mocks for block tests

--- test/api/two/two_blocks.cpp
+++ test/api/two/two_blocks.cpp
@@ -1,7 +1,11 @@
 
 #include "gtest/gtest.h"
+#include "gmock/gmock.h"
 #include "arkClient.h"
+#include "mocks/mock_api.h"
 #include "utils/json.h"
+
+using testing::Return;
 
 /* test_two_blocks_block
  * https://dexplorer.ark.io:8443/api/v2/blocks/13114381566690093367
@@ -38,12 +42,46 @@
  */
 TEST(api, test_two_block)
 {
-    Ark::Client::Connection<Ark::Client::API::Two> connection("167.114.29.55", 4003);
+    Ark::Client::Connection<MockApi> connection("167.114.29.55", 4003);
 
     auto apiVersion = connection.api.version();
     ASSERT_EQ(2, apiVersion);
 
-    const auto blockResponse = connection.api.blocks.get("13114381566690093367");
+    const std::string expected_response = R"({
+        "data": {
+            "id": "58328125061111756",
+            "version": 0,
+            "height": 3035362,
+            "previous": "3741191868092856237",
+            "forged": {
+                "reward": 200000000,
+                "fee": 0,
+                "total": 200000000
+            },
+            "payload": {
+                "hash": "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855",
+                "length": 0
+            },
+            "generator": {
+                "username": "genesis_6",
+                "address": "D5e2FzTPqdEHridjzpFZCCVyepAu6Vpmk4",
+                "publicKey": "023e577a7b3362e0aba70e6911d230e86d729b4cb640f0e0b25637b812a3e38b53"
+            },
+            "signature": "3044022047aeb0c9cfbb5709aba4c177009bfdc7804ef597073fb9ca6cb614d7e3d1af2d02207234119d02ca26600ece045c59266945081b4c8237370576aaad7c61a09fe0ad",
+            "transactions": 0,
+            "timestamp": {
+                "epoch": 32816544,
+                "unix": 1522917744,
+                "human": "2018-04-05T08:42:24Z"
+            }
+        }
+    })";
+
+    EXPECT_CALL(connection.api.blocks, get("58328125061111756"))
+      .Times(1)
+      .WillOnce(Return(expected_response));
+
+    const auto blockResponse = connection.api.blocks.get("58328125061111756");
 
     DynamicJsonBuffer jsonBuffer(blockResponse.size());
     JsonObject& root = jsonBuffer.parseObject(blockResponse);
@@ -145,12 +183,48 @@ TEST(api, test_two_block)
  */
 TEST(api, test_two_block_transactions)
 {
-    Ark::Client::Connection<Ark::Client::API::Two> connection("167.114.29.55", 4003);
+    Ark::Client::Connection<MockApi> connection("167.114.29.55", 4003);
 
     auto apiVersion = connection.api.version();
     ASSERT_EQ(2, apiVersion);
 
-    const auto blockTransactionsResponse = connection.api.blocks.transactions("9269622721511437262");
+    const std::string expected_response = R"({
+        "meta": {
+            "count": 1,
+            "pageCount": 1,
+            "totalCount": 1,
+            "next": null,
+            "previous": null,
+            "self": "/v2/blocks/14126007750611341900/transactions?page=1",
+            "first": "/v2/blocks/14126007750611341900/transactions?page=1",
+            "last": "/v2/blocks/14126007750611341900/transactions?page=1"
+        },
+        "data": [
+            {
+                "id": "57415c61e6e7f10a6f9820d5124b3916f3c3a036b360f4802f0eb484f86f3369",
+                "blockId": "14126007750611341900",
+                "type": 0,
+                "amount": 1000000000000000,
+                "fee": 10000000,
+                "sender": "DGihocTkwDygiFvmg6aG8jThYTic47GzU9",
+                "recipient": "DRac35wghMcmUSe5jDMLBDLWkVVjyKZFxK",
+                "signature": "3045022100878335a71ab6769f3c1e2895041ad24d6c58cdcfe1151c639e65289e5287b0a8022010800bcfdc3223a9c59a6b014e8adf72f1c34df8a46afe655b021930b03e214e",
+                "vendorField": "yo",
+                "confirmations": 3034848,
+                "timestamp": {
+                    "epoch": 3909196,
+                    "unix": 1494010396,
+                    "human": "2017-05-05T18:53:16Z"
+                }
+            }
+        ]
+    })";
+
+    EXPECT_CALL(connection.api.blocks, transactions("14126007750611341900"))
+      .Times(1)
+      .WillOnce(Return(expected_response));
+
+    const auto blockTransactionsResponse = connection.api.blocks.transactions("14126007750611341900");
 
     DynamicJsonBuffer jsonBuffer(blockTransactionsResponse.size());
     JsonObject& root = jsonBuffer.parseObject(blockTransactionsResponse);
@@ -253,86 +327,56 @@ TEST(api, test_two_block_transactions)
  */
 TEST(api, test_two_blocks)
 {
-    Ark::Client::Connection<Ark::Client::API::Two> connection("167.114.29.55", 4003);
+    Ark::Client::Connection<MockApi> connection("167.114.29.55", 4003);
 
     auto apiVersion = connection.api.version();
     ASSERT_EQ(2, apiVersion);
 
-    const auto blocksResponse = connection.api.blocks.all();
-
-    DynamicJsonBuffer jsonBuffer(blocksResponse.size());
-    JsonObject& root = jsonBuffer.parseObject(blocksResponse);
-
-    JsonObject& meta = root["meta"];
-
-    int count = meta["count"];
-    ASSERT_NE(0, count);
-
-    int pageCount = meta["pageCount"];
-    ASSERT_NE(0, pageCount);
-
-    int totalCount = meta["totalCount"];
-    ASSERT_NE(0, totalCount);
-
-
-    JsonObject& dataZero = root["data"][0];
-
-    int version = dataZero["version"];
-    ASSERT_EQ(0, version);
-}
-
-/* test_two_blocks_blocks_limit_page
- * https://dexplorer.ark.io:8443/api/v2/blocks?limit=10&page=1
- * Expected Response:
-    {
+    const std::string expected_response = R"({
         "meta": {
-            "count": int,
-            "pageCount": int,
-            "totalCount": int,
-            "next": null,
+            "count": 2,
+            "pageCount": 1517682,
+            "totalCount": 3035363,
+            "next": "/v2/blocks?limit=2&page=2",
             "previous": null,
-            "self": "\api/v2/blocks?limit=10&page=1",
-            "first": "/api/v2/blocks?limit=10&page=1",
-            "last": "/api/v2/blocks?limit=10&page=1"
+            "self": "/v2/blocks?limit=2&page=1",
+            "first": "/v2/blocks?limit=2&page=1",
+            "last": "/v2/blocks?limit=2&page=1517682"
         },
-        "data":
-        [
+        "data": [
             {
-                "id": "string",
-                "version": int,
-                "height": int,
-                "previous": "string",
+                "id": "6402736103893238690",
+                "version": 0,
+                "height": 3035363,
+                "previous": "58328125061111756",
                 "forged": {
-                    "reward": uint64_t,
-                    "fee": uint64_t,
-                    "total": uint64_t
+                    "reward": 200000000,
+                    "fee": 0,
+                    "total": 200000000
                 },
                 "payload": {
-                    "hash": "string",
-                    "length": ing
+                    "hash": "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855",
+                    "length": 0
                 },
                 "generator": {
-                    "username": "string",
-                    "address": "string",
-                    "publicKey": "string"
+                    "username": "shawmishrak",
+                    "address": "D7P41dV7s259L3P7BVPNyqExqNDC7vdfx9",
+                    "publicKey": "030fa94238eb63db0247a9bd6a3fd810f690b449ee9ce4eb654b94b22875a9a612"
                 },
-                "signature": "string",
-                "transactions": ing,
+                "signature": "304402204d0dbeb4e71a99a0f128a3480350014f0a9f250818dae908edd15bce99f49be00220257bf240c5d8578e9ffe144e7dbf0c2259d34e6571e6a83402edc01daec6228e",
+                "transactions": 0,
                 "timestamp": {
-                    "epoch": int,
-                    "unix": int,
-                    "human": "string"
+                    "epoch": 32816552,
+                    "unix": 1522917752,
+                    "human": "2018-04-05T08:42:32Z"
                 }
             }
         ]
-    }
- */
-TEST(api, test_two_blocks_limit_page)
-{
-    Ark::Client::Connection<Ark::Client::API::Two> connection("167.114.29.55", 4003);
+    })";
 
-    auto apiVersion = connection.api.version();
-    ASSERT_EQ(2, apiVersion);
+    EXPECT_CALL(connection.api.blocks, all(5, 1))
+      .Times(1)
+      .WillOnce(Return(expected_response));
 
     const auto blocksResponse = connection.api.blocks.all(5, 1);
 
@@ -404,11 +448,48 @@ TEST(api, test_two_blocks_limit_page)
  */
 TEST(api, test_two_blocks_search)
 {
-    Ark::Client::Connection<Ark::Client::API::Two> connection("167.114.29.55", 4003);
+    Ark::Client::Connection<MockApi> connection("167.114.29.55", 4003);
 
     auto apiVersion = connection.api.version();
     ASSERT_EQ(2, apiVersion);
 
+    const std::string expected_response = R"({
+        "meta": {
+            "count": 1,
+            "pageCount": 1,
+            "totalCount": 1,
+            "next": null,
+            "previous": null,
+            "self": "/v2/blocks/14126007750611341900/transactions/search?page=1",
+            "first": "/v2/blocks/14126007750611341900/transactions/search?page=1",
+            "last": "/v2/blocks/14126007750611341900/transactions/search?page=1"
+        },
+        "data": [
+            {
+                "id": "57415c61e6e7f10a6f9820d5124b3916f3c3a036b360f4802f0eb484f86f3369",
+                "blockId": "14126007750611341900",
+                "type": 0,
+                "amount": 1000000000000000,
+                "fee": 10000000,
+                "sender": "DGihocTkwDygiFvmg6aG8jThYTic47GzU9",
+                "recipient": "DRac35wghMcmUSe5jDMLBDLWkVVjyKZFxK",
+                "signature": "3045022100878335a71ab6769f3c1e2895041ad24d6c58cdcfe1151c639e65289e5287b0a8022010800bcfdc3223a9c59a6b014e8adf72f1c34df8a46afe655b021930b03e214e",
+                "vendorField": "yo",
+                "confirmations": 3034848,
+                "timestamp": {
+                    "epoch": 3909196,
+                    "unix": 1494010396,
+                    "human": "2017-05-05T18:53:16Z"
+                }
+            }
+        ]
+    })";
+
+    EXPECT_CALL(connection.api.blocks, search(std::make_pair("id", "8337447655053578871"), 5, 1))
+      .Times(1)
+      .WillOnce(Return(expected_response));
+
+    const auto walletsSearch = connection.api.blocks.search(std::make_pair("id", "8337447655053578871"), 5, 1);
     const std::map<std::string, std::string> body = {
       {"id", "8337447655053578871"},
       {"previousBlock", "6440284271011893973"},
