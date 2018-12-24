c08b7bc334cbb13059ed3e3c0a177805367b42d7
Chris <chrisjohnsonmail@gmail.com>
2018-12-24 10:33:58 -0600

    refactor:  Add initial mocks for votes tests

--- test/api/two/two_votes.cpp
+++ test/api/two/two_votes.cpp
@@ -1,7 +1,12 @@
-
 #include "gtest/gtest.h"
+#include "gmock/gmock.h"
+
+#include "mocks/mock_api.h"
+
 #include "arkClient.h"
 #include "utils/json.h"
+
+using testing::Return;
 
 /* test_two_vote
  * https://dexplorer.ark.io:8443/api/v2/votes/d202acbfa947acac53ada2ac8a0eb662c9f75421ede3b10a42759352968b4ed2
@@ -32,12 +37,40 @@
  */
 TEST(api, test_two_vote)
 {
-    Ark::Client::Connection<Ark::Client::API::Two> connection("167.114.29.55", 4003);
+    Ark::Client::Connection<MockApi> connection("167.114.29.55", 4003);
 
     auto apiVersion = connection.api.version();
     ASSERT_EQ(2, apiVersion);
 
-    const auto vote = connection.api.votes.get("d202acbfa947acac53ada2ac8a0eb662c9f75421ede3b10a42759352968b4ed2");
+    const std::string response = R"({
+        "data": {
+            "id": "beb8dd43c640f562704090159154b2742afba7eacada9e8edee447e34e7675c6",
+            "blockId": "13661015019049808045",
+            "type": 3,
+            "amount": 0,
+            "fee": 100000000,
+            "sender": "DAp7JjULVgqzd4jLofkUyLRovHRPUTQwiZ",
+            "recipient": "DAp7JjULVgqzd4jLofkUyLRovHRPUTQwiZ",
+            "signature": "3045022100e9a743c5aa0df427f49af61d35fe617182479f7e8d368ce23b7ec43ab6d269c80220193aafd4ccb3eedbd76ded7ea99f31629013dc3af60540029fe98b274d42d284",
+            "asset": {
+                "votes": [
+                    "+032fe001dff675a6edfe3d0e51201b2900d3b5050a46d770306aefaa574c022672"
+                ]
+            },
+            "confirmations": 48189,
+            "timestamp": {
+                "epoch": 32338609,
+                "unix": 1522439809,
+                "human": "2018-03-30T19:56:49Z"
+            }
+        }
+    })";
+
+    EXPECT_CALL(connection.api.votes, get("beb8dd43c640f562704090159154b2742afba7eacada9e8edee447e34e7675c6"))
+      .Times(1)
+      .WillOnce(Return(response));
+
+    const auto vote = connection.api.votes.get("beb8dd43c640f562704090159154b2742afba7eacada9e8edee447e34e7675c6");
 
     DynamicJsonBuffer jsonBuffer(vote.size());
     JsonObject& root = jsonBuffer.parseObject(vote);
@@ -124,15 +157,55 @@ TEST(api, test_two_vote)
  */
 TEST(api, test_two_votes)
 {
-    Ark::Client::Connection<Ark::Client::API::Two> connection("167.114.29.55", 4003);
+    Ark::Client::Connection<MockApi> connection("167.114.29.55", 4003);
 
     auto apiVersion = connection.api.version();
     ASSERT_EQ(2, apiVersion);
 
-    const auto transactionUnconfirmed = connection.api.votes.all(5, 1);
+    const std::string response = R"({
+        "meta": {
+            "count": 2,
+            "pageCount": 658,
+            "totalCount": 1315,
+            "next": "/v2/votes?page=2",
+            "previous": null,
+            "self": "/v2/votes?page=1",
+            "first": "/v2/votes?page=1",
+            "last": "/v2/votes?page=658"
+        },
+        "data": [
+            {
+                "id": "560959e435cbf8eec60691890f3dd55d141e76077e1fe803f65d137c91099240",
+                "blockId": "12872155462883631430",
+                "type": 3,
+                "amount": 0,
+                "fee": 100000000,
+                "sender": "DAp7JjULVgqzd4jLofkUyLRovHRPUTQwiZ",
+                "recipient": "DAp7JjULVgqzd4jLofkUyLRovHRPUTQwiZ",
+                "signature": "30440220522eadff84b5b4b2fc6a3ef611bf093dbd0a06963c32c767ee28729898d0a1d302203f851594e5b2271a987e98daa4fc8b5f384fac65c41eb1c43739af2d4b5dc902",
+                "asset": {
+                    "votes": [
+                        "-032fe001dff675a6edfe3d0e51201b2900d3b5050a46d770306aefaa574c022672"
+                    ]
+                },
+                "confirmations": 39989,
+                "timestamp": {
+                    "epoch": 32414926,
+                    "unix": 1522516126,
+                    "human": "2018-03-31T17:08:46Z"
+                }
+            }
+        ]
+    })";
 
-    DynamicJsonBuffer jsonBuffer(transactionUnconfirmed.size());
-    JsonObject& root = jsonBuffer.parseObject(transactionUnconfirmed);
+    EXPECT_CALL(connection.api.votes, all(5, 1))
+      .Times(1)
+      .WillOnce(Return(response));
+
+    const auto votes = connection.api.votes.all(5, 1);
+
+    DynamicJsonBuffer jsonBuffer(votes.size());
+    JsonObject& root = jsonBuffer.parseObject(votes);
 
     JsonObject& meta = root["meta"];
 
