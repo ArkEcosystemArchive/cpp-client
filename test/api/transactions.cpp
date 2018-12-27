0890ec7efe9cffc31fdb83805faae3ac9356aa89
Chris <chrisjohnsonmail@gmail.com>
2018-12-27 11:55:34 -0600

    fix:  Fix transactions tests

--- test/api/transactions.cpp
+++ test/api/transactions.cpp
@@ -8,6 +8,7 @@
 #include "utils/json.h"
 
 using testing::Return;
+using testing::_;
 
 /* test_two_transactions_transaction
  * https://dexplorer.ark.io:8443/api/v2/transactions/b324cea5c5a6c15e6ced3ec9c3135a8022eeadb8169f7ba66c80ebc82b0ac850
@@ -62,7 +63,7 @@ TEST(api, test_transaction)
         }
     })";
 
-    EXPECT_CALL(connection.api.transactions, get("5c6ce775447a5acd22050d72e2615392494953bb1fb6287e9ffb3c33eaeb79aa"))
+    EXPECT_CALL(connection.api.transactions, get(_))
       .Times(1)
       .WillOnce(Return(response));
 
@@ -74,43 +75,43 @@ TEST(api, test_transaction)
     JsonObject& data = root["data"];
 
     const char* id = data["id"];
-    ASSERT_STREQ("b324cea5c5a6c15e6ced3ec9c3135a8022eeadb8169f7ba66c80ebc82b0ac850", id);
+    ASSERT_STREQ("5c6ce775447a5acd22050d72e2615392494953bb1fb6287e9ffb3c33eaeb79aa", id);
 
     const char* blockId = data["blockId"];
-    ASSERT_STREQ("4375573732170746923", blockId);
+    ASSERT_STREQ("4271682877946294396", blockId);
 
     int type = data["type"];
-    ASSERT_EQ(3, type);
+    ASSERT_EQ(0, type);
 
     uint64_t amount = data["amount"];
-    ASSERT_TRUE(amount == 0);
+    ASSERT_TRUE(amount == 32106400000);
 
     uint64_t fee = data["fee"];
-    ASSERT_TRUE(fee == 100000000);
+    ASSERT_TRUE(fee == 10000000);
 
     const char* sender = data["sender"];
-    ASSERT_STREQ("DKcFDN6mhLAheRAfmN6LT1e4AeyF1Fd9bY", sender);
+    ASSERT_STREQ("DDiTHZ4RETZhGxcyAi1VruCXZKxBFqXMeh", sender);
 
     const char* recipient = data["recipient"];
-    ASSERT_STREQ("DKcFDN6mhLAheRAfmN6LT1e4AeyF1Fd9bY", recipient);
+    ASSERT_STREQ("DQnQNoJuNCvpjYhxL7fsnGepHBqrumgsyP", recipient);
 
     const char* signature = data["signature"];
-    ASSERT_STREQ("3045022100dc27398f4f3a24e55dc1ee87900de988254daa3fed71e82f4d6ef85ed4f9d9f8022025d71158cc15672863b2263622026ec19fa9cc9d2e8c78fa79eb2d8f4ef45fc7", signature);
+    ASSERT_STREQ("3044022047c39f6f45a46a87f91ca867f9551dbebf0035adcfcbdc1370222c7a1517fc0002206fb5ecc10460e0352a8b626a508e2fcc76e39e490b0a2581dd772ebc8079696e", signature);
 
     int confirmations = data["confirmations"];
-    ASSERT_GT(confirmations, 0);
+    ASSERT_EQ(confirmations, 1928);
 
 
     JsonObject& timestamp = data["timestamp"];
 
     int epoch = timestamp["epoch"];
-    ASSERT_EQ(45024866, epoch);
+    ASSERT_EQ(32794053, epoch);
 
     int timestampUnix = timestamp["unix"];
-    ASSERT_EQ(1535126066, timestampUnix);
+    ASSERT_EQ(1522895253, timestampUnix);
 
     const char* human = timestamp["human"];
-    ASSERT_STREQ("2018-08-24T15:54:26.000Z", human);
+    ASSERT_STREQ("2018-04-05T02:27:33Z", human);
 }
 
 /* test_transactions_transaction_types
@@ -139,15 +140,15 @@ TEST(api, test_transaction_types)
 
     const std::string response = R"({
 			  "data": {
-			    "TRANSFER": 0,
-			    "SECOND_SIGNATURE": 1,
-			    "DELEGATE_REGISTRATION": 2,
-			    "VOTE": 3,
-			    "MULTI_SIGNATURE": 4,
-			    "IPFS": 5,
-			    "TIMELOCK_TRANSFER": 6,
-			    "MULTI_PAYMENT": 7,
-			    "DELEGATE_RESIGNATION": 8
+			    "Transfer": 0,
+			    "SecondSignature": 1,
+			    "DelegateRegistration": 2,
+			    "Vote": 3,
+			    "MultiSignature": 4,
+			    "Ipfs": 5,
+			    "TimelockTransfer": 6,
+			    "MultiPayment": 7,
+			    "DelegateResignation": 8
 			  }
 			})";
 
@@ -236,7 +237,7 @@ TEST(api, test_transaction_unconfirmed)
 			  }
 			})";
 
-    EXPECT_CALL(connection.api.transactions, getUnconfirmed("dummy"))
+    EXPECT_CALL(connection.api.transactions, getUnconfirmed(_))
       .Times(1)
       .WillOnce(Return(response));
 
@@ -245,16 +246,48 @@ TEST(api, test_transaction_unconfirmed)
     DynamicJsonBuffer jsonBuffer(transactionUnconfirmed.size());
     JsonObject& root = jsonBuffer.parseObject(transactionUnconfirmed);
 
-    JsonObject& meta = root["meta"];
+    JsonObject& data = root["data"];
 
-    int count = meta["count"];
-    ASSERT_GE(count, 0);
+    const char* id = data["id"];
+    ASSERT_STREQ("dummy", id);
 
-    int pageCount = meta["pageCount"];
-    ASSERT_GE(pageCount, 0);
+    const char* blockId = data["blockId"];
+    ASSERT_STREQ("dummy", blockId);
 
-    int totalCount = meta["totalCount"];
-    ASSERT_GE(totalCount, 0);
+    int type = data["type"];
+    ASSERT_EQ(0, type);
+
+    uint64_t amount = data["amount"];
+    ASSERT_TRUE(10000000ull == amount);
+
+    uint64_t fee = data["fee"];
+    ASSERT_TRUE(10000000ull == fee);
+
+    const char* sender = data["sender"];
+    ASSERT_STREQ("dummy", sender);
+
+    const char* recipient = data["recipient"];
+    ASSERT_STREQ("dummy", recipient);
+
+    const char* signature = data["signature"];
+    ASSERT_STREQ("dummy", signature);
+
+    const char* vendorField = data["vendorField"];
+    ASSERT_STREQ("dummy", vendorField);
+
+    int confirmations = data["confirmations"];
+    ASSERT_EQ(10, confirmations);
+
+    JsonObject& timestamp = data["timestamp"];
+
+    uint64_t epoch = timestamp["epoch"];
+    ASSERT_TRUE(40505460ull == epoch);
+
+    uint64_t unix = timestamp["unix"];
+    ASSERT_TRUE(1530606660ull == unix);
+
+    const char* human = timestamp["human"];
+    ASSERT_STREQ("2018-07-03T08:31:00Z", human);
 }
 
 /* test_transactions_transactions
@@ -292,7 +325,7 @@ TEST(api, test_transaction_unconfirmed)
     ]
     }
  */
-#if 0
+
 TEST(api, test_transactions)
 {
     Ark::Client::Connection<MockApi> connection("167.114.29.55", 4003);
@@ -331,7 +364,7 @@ TEST(api, test_transactions)
       ]
   })";
 
-    EXPECT_CALL(connection.api.transactions, all(2, 1)
+    EXPECT_CALL(connection.api.transactions, all(_, _))
       .Times(1)
       .WillOnce(Return(response));
 
@@ -360,7 +393,6 @@ TEST(api, test_transactions)
     uint64_t fee = dataZero["fee"];
     ASSERT_TRUE(fee >= 0);
 }
-#endif
 
 /* test_transactions_transactions_unconfirmed
  * https://dexplorer.ark.io:8443/api/v2/transactions/unconfirmed?limit=2&page=1
@@ -420,7 +452,7 @@ TEST(api, test_transactions_unconfirmed)
       ]
       })";
 
-    EXPECT_CALL(connection.api.transactions, allUnconfirmed(5, 1))
+    EXPECT_CALL(connection.api.transactions, allUnconfirmed(_, _))
       .Times(1)
       .WillOnce(Return(response));
 
@@ -441,37 +473,7 @@ TEST(api, test_transactions_unconfirmed)
     ASSERT_TRUE(totalCount >= 0);
 }
 
-/* test_two_transactions_transactions_search_all
- *
- * Expected Response:
- */
-TEST(api, test_two_transactions_search_all)
-{
-  Ark::Client::Connection<Ark::Client::Api> connection("167.114.29.55", 4003);
-
-  auto apiVersion = connection.api.version();
-  ASSERT_EQ(2, apiVersion);
-
-  const std::map<std::string, std::string> body = { };
-  const auto transactions = connection.api.transactions.search(body, 5, 1);
-
-  DynamicJsonBuffer jsonBuffer(transactions.size());
-  JsonObject& root = jsonBuffer.parseObject(transactions);
-
-  JsonObject& meta = root["meta"];
-
-  int count = meta["count"];
-  ASSERT_TRUE(count >= 0);
-
-  int pageCount = meta["pageCount"];
-  ASSERT_TRUE(pageCount >= 0);
-
-  int totalCount = meta["totalCount"];
-  ASSERT_TRUE(totalCount >= 0);
-}
-
-#if 0 // TODO
-/* test_two_transactions_transactions_search
+/* test_transactions_transactions_search
  * 
  * Expected Response:
  {
@@ -544,15 +546,14 @@ TEST(api, test_transactions_search)
 			  ]
 			})";
 
-    EXPECT_CALL(connection.api.transactions, search(5, 1))
+    EXPECT_CALL(connection.api.transactions, search(_, _, _))
       .Times(1)
       .WillOnce(Return(response));
 
-    const auto transactions = connection.api.transactions.get("4bbc5433e5a4e439369f1f57825e92d07cf9cb8e07aada69c122a2125e4b9d48", 5, 1);
-    const std::map<std::string, std::string> body = {
-      {"id", "927ab6da141cc4fa9f1a4b5765ee9ecdf92d47a9cd3aada35aa136ad7d3d3e37"}
-    };
-    const auto transactions = connection.api.transactions.search(body, 5, 1);
+  const std::map<std::string, std::string> body = {
+    {"id", "dummy"}
+  };
+  const auto transactions = connection.api.transactions.search(body, 5, 1);
 
 	DynamicJsonBuffer jsonBuffer(transactions.size());
 	JsonObject& root = jsonBuffer.parseObject(transactions);
@@ -571,18 +572,20 @@ TEST(api, test_transactions_search)
 	JsonObject& data = root["data"][0];
 
 	const char* id = data["id"];
-	ASSERT_STREQ("927ab6da141cc4fa9f1a4b5765ee9ecdf92d47a9cd3aada35aa136ad7d3d3e37", id);
+	ASSERT_STREQ("dummy", id);
 
-	int version = data["version"];
-	ASSERT_EQ(1, version);
+  const char* blockId = data["blockId"];
+  ASSERT_STREQ("dummy", blockId);
+
+	int type = data["type"];
+	ASSERT_EQ(0, type);
 
 	const char* sender = data["sender"];
-	ASSERT_STREQ("DMdq8j6uzxErZxESGu7JfkaCFt3qx11fqj", sender);
+	ASSERT_STREQ("dummy", sender);
 
 	const char* recipient = data["recipient"];
-	ASSERT_STREQ("DMdq8j6uzxErZxESGu7JfkaCFt3qx11fqj", recipient);
+	ASSERT_STREQ("dummy", recipient);
 
 	const char* signature = data["signature"];
-	ASSERT_STREQ("3044022073337aefa7727cc1cf39e478ffe65cf5c66c5761b5848418d87307df250a68a5022053d4d5264b329e63cadad84fc6ec479fe5e430eaabd6177db1e9c283027ec2fa", signature);
+	ASSERT_STREQ("dummy", signature);
 }
-#endif
