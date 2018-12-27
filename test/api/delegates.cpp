6f81954d731b1c87f41786d50953297609376d2a
Chris <chrisjohnsonmail@gmail.com>
2018-12-27 11:06:09 -0600

    fix:  Fix delegates tests

--- test/api/delegates.cpp
+++ test/api/delegates.cpp
@@ -6,6 +6,7 @@
 #include "utils/json.h"
 
 using testing::Return;
+using testing::_;
 
 /* test_two_delegates_delegate
  * https://dexplorer.ark.io:8443/api/v2/delegates/boldninja
@@ -69,7 +70,7 @@ TEST(api, test_delegate)
         }
     })";
 
-    EXPECT_CALL(connection.api.delegates, get("boldninja"))
+    EXPECT_CALL(connection.api.delegates, get(_))
       .Times(1)
       .WillOnce(Return(expected_response));
 
@@ -84,34 +85,50 @@ TEST(api, test_delegate)
     ASSERT_STREQ("boldninja", username);
 
     const char* address = data["address"];
-    ASSERT_STREQ("DKrACQw7ytoU2gjppy3qKeE2dQhZjfXYqu", address);
+    ASSERT_STREQ("DARiJqhogp2Lu6bxufUFQQMuMyZbxjCydN", address);
 
     const char* publicKey = data["publicKey"];
-    ASSERT_STREQ("023ee98f453661a1cb765fd60df95b4efb1e110660ffb88ae31c2368a70f1f7359", publicKey);
+    ASSERT_STREQ("022cca9529ec97a772156c152a00aad155ee6708243e65c9d211a589cb5d43234d", publicKey);
 
     uint64_t votes = data["votes"];
-    ASSERT_GE(votes, 0);
+    ASSERT_EQ(0, votes);
 
     int rank = data["rank"];
-    ASSERT_NE(0, rank);
+    ASSERT_EQ(29, rank);
 
 
     JsonObject& blocks = data["blocks"];
 
     int produced = blocks["produced"];
-    ASSERT_NE(0, produced);
+    ASSERT_EQ(0, produced);
 
     int missed = blocks["missed"];
-    ASSERT_GE(missed, 0);
+    ASSERT_EQ(0, missed);
+
+    JsonObject& last = data["last"];
+
+    const char* last_id = last["id"];
+    ASSERT_STREQ("10652480998435361357", last_id);
+
+    JsonObject& timestamp = last["timestamp"];
+
+    uint64_t epoch = timestamp["epoch"];
+    ASSERT_TRUE(32816112ull == epoch);
+
+    uint64_t unix = timestamp["unix"];
+    ASSERT_TRUE(1522917312ull == unix);
+
+    const char* human = timestamp["human"];
+    ASSERT_STREQ("2018-04-05T08:35:12Z", human);
 
 
     JsonObject& production = data["production"];
 
     double approval = production["approval"];
-    ASSERT_NE(100.00, approval);
+    ASSERT_EQ(0.10, approval);
 
     double productivity = production["productivity"];
-    ASSERT_NE(100.00, productivity);
+    ASSERT_EQ(0.0, productivity);
 }
 
 /* test_delegates_delegate_blocks
@@ -215,7 +232,7 @@ TEST(api, test_delegate_blocks)
         ]
     })";
 
-    EXPECT_CALL(connection.api.delegates, blocks("boldninja", 3, 1))
+    EXPECT_CALL(connection.api.delegates, blocks(_, _, _))
       .Times(1)
       .WillOnce(Return(expected_response));
 
@@ -227,34 +244,34 @@ TEST(api, test_delegate_blocks)
     JsonObject& meta = root["meta"];
 
     int count = meta["count"];
-    ASSERT_NE(0, count);
+    ASSERT_EQ(2, count);
 
     int pageCount = meta["pageCount"];
-    ASSERT_NE(0, pageCount);
+    ASSERT_EQ(29919, pageCount);
 
     int totalCount = meta["totalCount"];
-    ASSERT_NE(0, totalCount);
+    ASSERT_EQ(59838, totalCount);
 
     const char* id = root["data"][0]["id"];
-    ASSERT_STRNE("", id);
+    ASSERT_STREQ("10652480998435361357", id);
 
     int version = root["data"][0]["version"];
     ASSERT_EQ(0, version);
 
     uint64_t height = root["data"][0]["height"];
-    ASSERT_TRUE(height >= 0);
+    ASSERT_TRUE(3035318ull == height);
 
     const char* previous = root["data"][0]["previous"];
-    ASSERT_STRNE("0", previous);
+    ASSERT_STREQ("12548322724277171379", previous);
 
     const char* username = root["data"][0]["generator"]["username"];
     ASSERT_STREQ("boldninja", username);
 
     const char* address = root["data"][0]["generator"]["address"];
-    ASSERT_STREQ("DKrACQw7ytoU2gjppy3qKeE2dQhZjfXYqu", address);
+    ASSERT_STREQ("DARiJqhogp2Lu6bxufUFQQMuMyZbxjCydN", address);
 
     const char* publicKey = root["data"][0]["generator"]["publicKey"];
-    ASSERT_STREQ("023ee98f453661a1cb765fd60df95b4efb1e110660ffb88ae31c2368a70f1f7359", publicKey);
+    ASSERT_STREQ("022cca9529ec97a772156c152a00aad155ee6708243e65c9d211a589cb5d43234d", publicKey);
 }
 
 
@@ -311,7 +328,7 @@ TEST(api, test_delegate_voters)
         ]
     })";
 
-    EXPECT_CALL(connection.api.delegates, voters("boldninja", 5, 1))
+    EXPECT_CALL(connection.api.delegates, voters(_, _, _))
       .Times(1)
       .WillOnce(Return(expected_response));
 
@@ -335,13 +352,13 @@ TEST(api, test_delegate_voters)
     JsonObject& dataZero = root["data"][0];
 
     const char* address = dataZero["address"];
-    ASSERT_STRNE("", address);
+    ASSERT_STREQ("D5mbS6mpP5UheuciNscpDLgC127kYjRtkK", address);
 
     const char* publicKey = dataZero["publicKey"];
-    ASSERT_STRNE("", publicKey);
+    ASSERT_STREQ("03f7e0b1ab14985990416f72ed0b206c20b9efa35156e4528c8ff749fa0eea5d5a", publicKey);
 
     bool isDelegate = dataZero["isDelegate"];
-    ASSERT_TRUE(isDelegate || !isDelegate);
+    ASSERT_FALSE(isDelegate);
 }
 
 /* test_delegates_delegates
@@ -453,14 +470,14 @@ TEST(api, test_delegates)
     JsonObject& dataZero = root["data"][0];
 
     const char* username = dataZero["username"];
-    ASSERT_STRNE("", username);
+    ASSERT_STREQ("dark_jmc", username);
 
     const char* address = dataZero["address"];
-    ASSERT_STRNE("", address);
+    ASSERT_STREQ("D5PXQVeJmchVrZFHL7cALZK8mWWzjCaVfz", address);
 
     const char* publicKey = dataZero["publicKey"];
-    ASSERT_STRNE("", publicKey);
+    ASSERT_STREQ("02a9a0ac34a94f9d27fd9b4b56eb3c565a9a3f61e660f269775fb456f7f3301586", publicKey);
 
     uint64_t votes = dataZero["votes"];
-    ASSERT_TRUE(votes >= 0);
+    ASSERT_TRUE(0ull == votes);
 }
