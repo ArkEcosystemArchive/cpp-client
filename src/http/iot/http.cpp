379c952696915194e67c49d6e18dd48a7c7c3971
Brian Faust <faustbrian@users.noreply.github.com>
2019-01-15 11:18:01 +0200

    fix: use the correct API version header

--- src/http/iot/http.cpp
+++ src/http/iot/http.cpp
@@ -42,7 +42,7 @@ class PlatformHTTP : public AbstractHTTP
         /* Bad HTTP GET.\nRetrying connection.. */
         delay(1000);
         httpClient.addHeader("Content-Type", "application/json");
-        httpClient.addHeader("Accept", "application/vnd.core-api.v2+json");
+        httpClient.addHeader("API-Version", "2");
         httpClient.begin(request);
         code = httpClient.GET();
         count++;
