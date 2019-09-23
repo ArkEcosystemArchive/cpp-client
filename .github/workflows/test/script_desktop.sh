e1977ef4827e3fa84a98337849b5e32c0c1e6b1d
Brian Faust <faustbrian@users.noreply.github.com>
2019-09-23 07:17:32 +0300

    ci: setup github action workflow for testing (#129)

--- .github/workflows/test/script_desktop.sh
+++ .github/workflows/test/script_desktop.sh
@@ -1,15 +1,13 @@
-#!/bin/bash
-
 # run desktop builds
-mkdir build
-cd build
-cmake .. -DCMAKE_BUILD_TYPE=Coverage -DBUILD_TESTING=OFF
+cmake . -DCMAKE_BUILD_TYPE=Coverage -DBUILD_TESTING=OFF
 cmake --build .
 
+# disable build examples until breaking changes are committed so the example can pull them in
 # build examples
-cd ../examples/cmake_example
-./build.sh
+#cd ./examples/cmake_example
+#cmake .
+#cmake --build .
 
 # run Gtest
-cd ../../build
+# cd ../../
 ./test/Ark-Cpp-Client-tests
