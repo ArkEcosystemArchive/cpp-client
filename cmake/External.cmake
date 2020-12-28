cfed4fe7b7011961f45558fdeb051c3f2b617de5
sleepdefic1t <sleepdefic1t@users.noreply.github.com>
2020-12-28 14:22:21 -0500

    ci: fix windows gh actions
    resolves #130

--- cmake/External.cmake
+++ cmake/External.cmake
@@ -45,4 +45,11 @@ set(CURL_INCLUDE_DIR
     ${EXTERNAL_LIBRARY_DIR}/curl/src/include
     CACHE INTERNAL "cURL: Include Folder Path")
 
+if (MSVC)
+    set(CURL_LIBRARY ${EXTERNAL_LIBRARY_DIR}/curl/src/build/lib)
+    find_package(CURL)
+    include_directories(${CURL_INCLUDE_DIRS})
+    link_directories(${CURL_LIBRARIES})
+endif()
+
 # ------------------------------------------------------------------------------
